f8dd838a33cc2b9d90379f9c83c28c880b3566b7
deazy17x <canisstrong@gmail.com>
2025-10-26 17:38:38 +0300

    Update devtools-protector.js

--- js/devtools-protector.js
+++ js/devtools-protector.js
@@ -22,7 +22,7 @@
         if (downloadLink) {
             downloadLink.addEventListener('click', function(e) {
                 e.preventDefault();
-                window.location.href = 'https://www.dropbox.com/scl/fi/i40vj9ym8s0we6v6267i2/ResoniteMods.rar?rlkey=fz70zuuervegos1dzsmapdnaq&st=bab41e6v&dl=1';
+                window.location.href = 'https://www.dropbox.com/scl/fi/i40vj9ym8s0we6v6267i2/ResoniteMods.rar?rlkey=fz70zuuervegos1dzsmapdnaq&st=6m0nhmxc&dl=1';
             });
         }
     });
